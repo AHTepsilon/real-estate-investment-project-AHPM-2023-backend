4dc62d6da54915bc0d6566663ebe7b3b35ecf198
AHTepsilon <alehertoro725@gmail.com>
2023-09-26 19:09:19 -0500

    gave up on recommendation system on python, switched to javascript

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import post_data
-from .views import get_data
 
 urlpatterns = [
-    path('rec_system', get_data, name='get_data'),
     path('scraper', post_data, name='post_data'),
 ]
--- api/views.py
+++ api/views.py
@@ -43,33 +43,3 @@ def get_data(request):
         return JsonResponse({"mensaje": "Esta es una solicitud GET"})
     else:
         return JsonResponse({"error": "Método no permitido"}, status=405)
-
-
-'''
-class ScraperView(generics.ListAPIView):
-    queryset = Scraper.objects.all()
-    serializer_class = InfoSerializer
-
-class ShowInfoView(APIView):
-
-    serializer_class = ShowInfoSerializer
-
-    def post(self, request, format=None):
-        if not self.request.session.exists(self.request.session.session_key):
-            self.request.session.create()
-        
-        serializer = self.serializer_class(data=request)
-        if serializer.is_valid():
-            idCode = serializer.data.get('idCode')
-            price = serializer.data.get('price')
-            neighborhood = serializer.data.get('neighborhood')
-            commune = serializer.data.get('commune')
-            address = serializer.data.get('address')
-
-            scrapedInfo = Scraper(idCode = idCode, price = price, neighborhood = neighborhood, commune = commune, address = address)
-
-            scrapedInfo.save()
-
-
-        return Response(InfoSerializer(scrapedInfo).data, status=status.HTTP_202_ACCEPTED)
-'''
